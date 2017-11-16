ca0778a7015c76168cff7e92619e3796818ecd00
Joshua Elder <aresx130@gmail.com>
2017-11-16 10:46:35 -0800

    Added basic functions for the edit book feature.

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(() => {
 
   const listBooks = () => {
-    window.fetch('https://mutably.herokuapp.com/books', {
+    fetch('https://mutably.herokuapp.com/books', {
       method: 'get',
     }).then((apiRes) => {
       return apiRes.json()
@@ -24,10 +24,10 @@ $(document).ready(() => {
              <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                  <img src='${image}' class="book-image">
              </div>
-                 <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                   <span class="book-title">Title: ${title}</span>
-                   <span class="book-author">Author: ${author}</span>
-                   <span class="book-releaseDate">Release Date: ${releaseDate}</span>
+                 <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 book-text">
+                   <span class="book-title book-info">Title: ${title}</span>
+                   <span class="book-author book-info">Author: ${author}</span>
+                   <span class="book-releaseDate book-info">Release Date: ${releaseDate}</span>
                  </div>
              </div>
              <button type="submit" class="btn btn-block btn-info btn-edit">Edit</button>
@@ -60,26 +60,74 @@ $(document).ready(() => {
       });
   }
 
+const editBook = (id, bookObject) => {
+    return fetch(`https://mutably.herokuapp.com/books/${id}`, {
+      method: 'put',
+      body: JSON.stringify(bookObject),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then((response) => {
+      return response.json()
+        // if (response.ok) {
+        //   return response.body
+        // }
+    }).then((body) => {
+      return body
+    })
+}
+
+
+
   $('#addBook').on({
-    click: function(e){
-      if($('#title').val() && $('#author').val() && $('#releaseDate').val() && $('#image').val()){
-         addBookAjax(e)
-       } else {
+    click: function(e) {
+      if ($('#title').val() && $('#author').val() && $('#releaseDate').val() && $('#image').val()) {
+        addBookAjax(e)
+      } else {
         $( ".add-book" ).append('<p>Must enter a movie</p>')
-       }
-    }
+      }
+  },
   });
 
-$(document).on('click','.btn-edit', function () {
+$('.list-group').on('click','.btn-edit', function () {
   const book = $(this).parent().parent()
-  const bookId = book.attr('id')
-$(this).replaceWith('<button type="submit" class="btn btn-block btn-success btn-save">Save</button>')
-book.find('.book-title').replaceWith('<input type="text" name="editTitle" required placeholder="Enter book title" id="edit-title">')
-book.find('.book-author').replaceWith('<input type="text" name="editAuthor" required placeholder="Enter book author" id="edit-author">')
-book.find('.book-releaseDate').replaceWith('<input type="text" name="editReleaseDate" required placeholder="Enter book release date" id="editReleaseDate">')
+  const bookTitle = book.find('.book-title').text().slice(7)
+  const bookAuthor = book.find('.book-author').text().slice(8)
+  const bookRelease = book.find('.book-releaseDate').text().slice(14)
+  const bookImageUrl = book.find('.book-image').attr('src')
 
-  // book.querySelectorAll('.book-title')
+    $(this).replaceWith('<button type="submit" class="btn btn-block btn-success btn-save">Save</button>')
+    book.find('.book-title').replaceWith(`<input type="text" name="editTitle" required value="${bookTitle}" id="edit-title" class="edit-text">`)
+    book.find('.book-author').replaceWith(`<input type="text" name="editAuthor" required value="${bookAuthor}" id="edit-author" class="edit-text">`)
+    book.find('.book-releaseDate').replaceWith(`<input type="text" name="editReleaseDate" required value="${bookRelease}" id="edit-releaseDate" class="edit-text">`)
+    book.find('.book-text').append(`<input type="text" name="editImage" required value="${bookImageUrl}" id="edit-image" class="edit-text">`)
 })
 
+$('.list-group').on('click','.btn-save', function () {
+  const book = $(this).parent().parent()
+  const bookId = book.attr('id')
+  const editTitle = book.find('#edit-title').val()
+  const editAuthor = book.find('#edit-author').val()
+  const editRelease = book.find('#edit-releaseDate').val()
+  const editImage = book.find('#edit-image').val()
+
+  const bookObject = {
+    _id: bookId,
+    title: editTitle,
+    author: editAuthor,
+    image: editImage,
+    releaseDate: editRelease
+  }
+
+  const bookData = editBook(bookId, bookObject)
+  console.log(bookData)
+})
+
+$(document).on('click', '.btn-delete', function() {
+  const book = $(this).parent().parent()
+  const bookId = book.attr('id')
+  $(book).empty()
+})
 
 });
